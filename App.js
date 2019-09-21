dd4a95bb1bcc1d3a2050e80a3a184fcbcb0b0b6b
18vijayb <18vijayb@students.harker.org>
2019-09-20 21:29:50 -0500

    Modified app.js to display buttons and logo

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image, Alert } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default function App() {
+  let pic = {
+    uri: './assets/ccaclogo.jpeg'
+  };
   return (
     <View style={styles.container}>
+      <Image source={require('./assets/ccaclogo.jpg')} style={{width: 400, height: 300}}/>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Parents"
+        color="#fff"
+        style={{right: 200}}
+        onPress={() => Alert.alert('You picked parents!')}
+      />
+      <Button
+        title="Children"
+        color="#fff"
+        style={{left: 200}}
+        onPress={() => Alert.alert('You picked children!')}
+      />
     </View>
   );
 }
@@ -12,7 +29,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8E44AD',
     alignItems: 'center',
     justifyContent: 'center',
   },
