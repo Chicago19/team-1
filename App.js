6bbd167ef1162683f2100a0ed95b8ccdc6562e8c
James Wei <jameswei2000@gmail.com>
2019-09-21 00:25:36 -0500

    nav

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome',
+    };
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <Button
+                title="Go to Jane's profile"
+                onPress={() => navigate('Profile', { name: 'Jane' })}
+            />
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+class ProfileScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome2',
+    };
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <Button
+                title="Go to Home profile"
+                onPress={() => navigate('Home', { name: 'Jane' })}
+            />
+        );
+    }
+}
+
+const MainNavigator = createStackNavigator({
+    Home: { screen: HomeScreen },
+    Profile: { screen: ProfileScreen },
 });
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # team-1
+
+<!-- Navigation 
+npm install --save react-navigation
+npm install --save react-native-gesture-handler
+npm install --save react-navigation-stack -->
+Navigation
+npm install react-router-native
+
+PDF
+npm install react-native-view-pdf --save
